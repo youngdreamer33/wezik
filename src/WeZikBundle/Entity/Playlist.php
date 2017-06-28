adc6800d6407ae9e9f78bec38723b409ff4a85b9
michael33610 <michael.behrens33@gmail.com>
2017-06-28 12:02:37 +0200

    Entités

---
+++ src/WeZikBundle/Entity/Playlist.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace WeZikBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Playlist
+ *
+ * @ORM\Table(name="playlist")
+ * @ORM\Entity(repositoryClass="WeZikBundle\Repository\PlaylistRepository")
+ */
+class Playlist
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="visibilite", type="boolean")
+     */
+    private $visibilite;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Playlist
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set visibilite
+     *
+     * @param boolean $visibilite
+     *
+     * @return Playlist
+     */
+    public function setVisibilite($visibilite)
+    {
+        $this->visibilite = $visibilite;
+
+        return $this;
+    }
+
+    /**
+     * Get visibilite
+     *
+     * @return bool
+     */
+    public function getVisibilite()
+    {
+        return $this->visibilite;
+    }
+}
+
