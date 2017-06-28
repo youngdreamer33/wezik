6b2baeccb39f1876bc0e8b0264b3b01893f6987c
ia@shinoa.tv <ia@shinoa.tv>
2017-06-28 14:05:33 +0200

    Ajout des fichiers Angular dans /web Affichage du var_dump $user .

--- src/WeZikBundle/Controller/DefaultController.php
+++ src/WeZikBundle/Controller/DefaultController.php
@@ -3,11 +3,23 @@
 namespace WeZikBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use WeZikBundle\Entity\User;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        return $this->render('WeZikBundle:Default:index.html.twig');
+    }
+
+    public function converToJSONAction()
+    {
+
+        //$user = New User();
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository(User::class)->findAll();
+        var_dump($user);
+        $myJSON = json_encode($user);
         return $this->render('WeZikBundle:Default:index.html.twig');
     }
 }
