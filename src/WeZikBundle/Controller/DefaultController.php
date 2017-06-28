98f17be2a974c620d582df28656a10185a8b289e
ia@shinoa.tv <ia@shinoa.tv>
2017-06-28 14:23:49 +0200

    var_dump $user with comments

--- src/WeZikBundle/Controller/DefaultController.php
+++ src/WeZikBundle/Controller/DefaultController.php
@@ -15,13 +15,16 @@ class DefaultController extends Controller
     public function converToJSONAction()
     {
 
-        //$user = New User();
+        //recupération de l'USER
         $em = $this->getDoctrine()->getManager();
         $user = $em->getRepository(User::class)->findAll();
+        // convertir l'objet USER en tableau
         $arrayUser = (array) $user;
         var_dump((array) $user);
+        // convertir l'objet en JSON
         $myJSON = json_encode($arrayUser);
         var_dump($myJSON);
+        // decoder le JSON
         $decode=json_decode($myJSON);
         var_dump($decode);
         return $this->render('WeZikBundle:Default:index.html.twig');
